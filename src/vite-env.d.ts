0ab4632cc643df2c8f8227d4c3138289567372b6
wendelrb <wendelbarbaro@gmail.com>
2025-07-19 20:07:00 -0300

    update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
-/// <reference types="vite/client" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/novo-horizonte-web-79/', 
+  plugins: [react()],
+})
